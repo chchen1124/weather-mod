2792247c08d4321645076628e63ae0e3421fe77f
John Desrosiers <johndrunner@yahoo.com>
2017-10-02 07:07:36 -0700

    initial commit

---
+++ weather.js
@@ -0,0 +1,8 @@
+// Dependencies
+// ======================================================================
+const Request = require("request-promise-native");
+const KEYS = require("keys.json");
+
+// Config globals
+// ======================================================================
+const API_TOKEN = KEYS.noaa;
